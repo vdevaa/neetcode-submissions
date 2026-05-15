3bb614046f3b598391ce26ee057df77b7ce1ea14
Vijay Deva <107094377+vdevaa@users.noreply.github.com>
2026-05-14 19:41:05 -0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagMap = defaultdict(list)
+        
+        for wrd in strs:
+
+            mapper = [0] * 26 # set as our key
+
+            for ch in wrd:
+
+                mapper[ord(ch) - ord('a')] += 1
+
+            anagMap[tuple(mapper)].append(wrd)
+
+
+        return list(anagMap.values())
+
